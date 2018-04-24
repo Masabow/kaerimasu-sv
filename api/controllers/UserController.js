7d64ef6dfd9ec101d5dcd2df9c22dd682858a506
masaya <m.fuyuhiro@gmail.com>
2018-04-24 23:43:15 +0900

    帰宅時間設定

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,7 +4,7 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-
+require('date-utils');
 module.exports = {
 	findOne: function (req, res) {
 		User.findOne({ id: 1 })
@@ -21,12 +21,19 @@ module.exports = {
 			      if (req.isSocket) {
 			        User.subscribe(req, [user.id]);
 			      }
-
+			      userRecord['status_name'] = '状態:';
 			      if (userRecord['status'] == 0) {
-			    	  userRecord['status_name'] = '異常なし';
+			    	  userRecord['status_name'] += '異常なし';
 			      } else {
-			    	  userRecord['status_name'] = '帰ります';
+			    	  userRecord['status_name'] += '帰ります';
 			      }
+			      var updateMs = userRecord['updatedAt'];
+			      var sec = userRecord['duration']['value'];
+			      var date = new Date(updateMs);
+			      userRecord['update_time'] = new Date(date).toFormat("YYYY/MM/DD HH24:MI:SS");
+			      date.setTime(date.getTime() + sec* 1000);
+			      userRecord['dist_time'] = date;
+			      userRecord['format_time'] = 'Arrive:' + date.toFormat("YYYY/MM/DD HH24:MI:SS");
 			      return res.ok(userRecord);
 			    });
 	}
